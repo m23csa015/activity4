6d7f3a78e8b80198fb1181fed7d9de79f16b3597
Mohit <m4mohit21@gmail.com>
2024-04-04 21:53:08 +0530

    change epochs

--- act4.py
+++ act4.py
@@ -31,7 +31,7 @@ def create_simple_cnn(input_shape, num_classes):
 input_shape = (28, 28, 1)  # Height, Width, Channels
 
 # Define the number of classes (e.g., for a 10-class classification task)
-num_classes = 10
+num_classes = 15
 
 # Create the CNN model
 simple_cnn_model = create_simple_cnn(input_shape, num_classes)
